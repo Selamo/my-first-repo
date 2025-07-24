827ed07b53000985cca07e8af67897bd57531aed
Selamo Allen <selamoallen52@gmail.com>
2025-07-23 17:15:16 -0700

    Getting started with variables

---
+++ 2.0 Software 1.0/Python Basics/1.0 day 1/variables.py
@@ -0,0 +1,21 @@
+# Read through the comments to understad variables
+print("Hello, Deepseeds")
+
+#why we need variables: for code reusability
+#Variables arre memory locations
+name =  "Allen"
+
+print("I am Gita, I love the name -Gita, it was given to me by my father, Paul Gita and I really like the name Gita.")
+
+print("I am " + name + ", I love the name -" + name + ", it was given ti me by my father, " + name + "Paul")
+
+#A vairiable
+#is a string if it is inside of single or double quotes
+name =  "Allen"
+#is a number and can be float(decimal) or integer(whole  number)
+age = 23 #integer
+height = 3.56 #float
+#is a boolean if it can either be true or false/ 0 or 1
+#this is a boolean
+isMarried = True
+#or it can be false
